f4ec514ac4c36206d7a360b752a9cf80a4767ea6
G9D4 <gldt2494@gmail.com>
2022-02-09 11:10:28 -0500

    agrega funcion conversion ruta absoluta

--- src/api.js
+++ src/api.js
@@ -1,2 +1,21 @@
-//  /^(.+)\\([^\/]+)$
-// regex para path absoluto
+const { resolve } = require('path')
+const path = require ('path')
+
+const regxSlash = /\\/g
+
+
+//Le damos formato a la ruta
+// const renderPath = (enteredPath) => {
+//     const filePath = enteredPath.replace(/\\/g, "/")
+//     return filePath
+// }
+
+//Verificamos si la ruta es absoluta
+
+const absolutePath = (filePath) => {
+    if(path.isAbsolute(filePath) === false){
+        return path.resolve(filePath)
+    }
+    return filePath
+}
+
