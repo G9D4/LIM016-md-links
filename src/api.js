8e29d2cebacb722c4b129a04a419ac0678b38e58
G9D4 <gldt2494@gmail.com>
2022-02-09 20:52:14 -0500

    mejora modularizacion y agrega funcion verificacion directorio

--- src/api.js
+++ src/api.js
@@ -2,6 +2,10 @@ const path = require ('path')
 const fs = require ('fs')
 
 const regxSlash = /\\/g
+const op1 = 'C:/Users/gabri/Desktop/laboratoria-md-links/LIM016-md-links/src/commands'
+const op2 = 'src/commands'
+const op3 = 'C:/Users/gabri/Desktop/laboratoria-md-links/LIM016-md-links/pruebas/prueba2.md'
+const op4 = 'pruebas/prueba2.md'
 
 
 //Le damos formato a la ruta
@@ -11,18 +15,25 @@ const regxSlash = /\\/g
 // }
 
 //Verificamos si la ruta es absoluta
-const absolutePath = (filePath) => {
-    if(path.isAbsolute(filePath) === false){
-        return path.resolve(filePath)
-    }
-    return filePath
+const isAbsolute = (filePath) => {
+    return path.isAbsolute(filePath)
+}
+
+//Convertimos la ruta relativa a absoluta
+const toAbsolute = (filePath) => {
+    return path.resolve(filePath)
 }
 
 //Verificamos si la ruta existe 
 //(de ser true, deberia retornar la ruta?)(por que cuando no existe da undefined al final?)
-const realPath = (filePath) => {
+const isrealPath = (filePath) => {
     if(fs.existsSync(filePath) === true){
         return filePath
     }
     return console.log("La ruta no existe")
+}
+
+//Verificamos si es un directorio
+const isDirectory = (filePath) => {
+        return fs.statSync(filePath).isDirectory()
 }
