b9df8e9e4c7f6bc7dddd8061b6554b0a39c52664
G9D4 <gldt2494@gmail.com>
2022-02-19 22:47:59 -0500

    cli ejecutable

--- src/cli.js
+++ src/cli.js
@@ -5,12 +5,7 @@ const stats = require ('./commands/stats')
 const argv = require ('yargs')
 
     .usage('Usage: md-links <path> [options]')
-    .example(
-        ["md-links your/path", "returns URL, link text and full file path"],
-        ["md-links your/path --validate", "returns URL, link text, full file path, status and ok/fail message"],
-        ["md-links your/path --stats", "returns number of total and unique links"],
-        ["md-links your/path --validate --stats", "returns values of both options plus number of broken links"]
-        )
+    .example("md-links your/path --validate --stats", "returns both values plus broken links")
         .options({
             validate: {
                 name: 'validate',
@@ -29,5 +24,13 @@ const argv = require ('yargs')
                 describe: 'retrieves statistics about the array of links'
             }
         })
+    .strictOptions(true)
+    .showHelpOnFail(false, 'Oops, you need --help?')
     .help('h')
-    .alias('h', 'help').argv
+    .alias('h', 'help')
+    .argv
+
+let path = argv._
+
+
+
